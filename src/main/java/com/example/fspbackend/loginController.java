3579e9cb37a6696a866f91ff66a485b65fa4494d
MinseokGo <rhalstjr1999@naver.com>
2023-05-27 15:55:40 +0900

    ㅁㄴㅇ

--- src/main/java/com/example/fspbackend/loginController.java
+++ src/main/java/com/example/fspbackend/loginController.java
@@ -1,11 +1,13 @@
 package com.example.fspbackend;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "rest/api/login")
+@CrossOrigin(origins = "http://localhost:8080")
 public class loginController {
     @PostMapping
     public void login(String id, String password) {
