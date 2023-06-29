dffe95b7ec4fc9587841079e3e8ffee2829238a5
MinseokGo <rhalstjr1999@naver.com>
2023-06-29 12:45:07 +0900

    update

--- src/main/java/com/example/fspbackend/Controller/WritePostController.java
+++ src/main/java/com/example/fspbackend/Controller/WritePostController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping(value = "rest/api/post")
 @CrossOrigin(origins = "http://localhost:8080")
-
 public class WritePostController {
     @Autowired
     private WritePostService writePostService;
